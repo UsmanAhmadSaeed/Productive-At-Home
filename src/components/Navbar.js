eb1b093dab22b7947c85f6406edabf02b4a0bf44
Usman Ahmad Saeed <20966983+UsmanAhmadSaeed@users.noreply.github.com>
2020-06-13 23:46:04 +0500

    before feature addition

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,15 +3,19 @@ import React, {Component} from "react"
 class Navbar extends Component {
     render(){
         return(
-        <nav className="navbar navbar-inverse navbar-fixed-top navbar-expand-lg bg-dark">
-            <div className="navbar-header">
-                <a className="navbar-brand" href="#">Productive at Home</a>
+        <nav className="navbar navbar-fixed-top navbar-expand-lg bg-success">
+            <div className="navbar-header mx-auto">
+                <a className="navbar-brand text-white" href="./">Productive at Home</a>
             </div>
-            <ul className="navbar navbar-nav" style={{color:"white"}}>
-                <li className="navbar-item">Home</li>
-                <li className="navbar-item">Tasks</li>
-                <li className="navbar-item">Rewards</li>
+            {/* <ul className="navbar navbar-nav mx-auto">
+                <li className="navbar-item"><a href="#">Home</a></li>
+                <li className="navbar-item"><a href="#">Tasks</a></li>
+                <li className="navbar-item"><a href="#">Rewards</a></li>
             </ul>
+            <ul className="navbar navbar-nav navbar-right" style={{color:"white"}}>
+                <li className="navbar-item"><a href="#">Login</a></li>
+                <li className="navbar-item"><a href="#">Register</a></li>
+            </ul> */}
         </nav>
         );
     }
